eddea8525e04dc1a7230b041afac317f13e9ba3e
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-24 19:37:20 +0100

    fixed bug on contact admin module

---
+++ apps/admin/modules/dmContactAdmin/lib/DmContactAdminForm.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * dmContactAdmin admin form
+ *
+ * @package    retest
+ * @subpackage dmContactAdmin
+ * @author     Your name here
+ */
+class DmContactAdminForm extends BaseDmContactForm
+{
+  public function configure()
+  {
+    parent::configure();
+
+    unset($this['body']);
+  }
+}
