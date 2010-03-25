9440125e330fb7139f9ef67592904b60acc0722e
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-03-25 19:58:22 +0100

    added default collection image - Closes #20

--- apps/front/modules/collection/templates/_default/photo.php
+++ apps/front/modules/collection/templates/_default/photo.php
@@ -1,6 +1,15 @@
 <?php
 
-echo _media($collection->User->Photo)
-->size(300, 300)
-->method('inflate')
-->set('.user_photo');
+if($collection->User->Photo)
+{
+  echo _media($collection->User->Photo)
+  ->size(300, 300)
+  ->method('inflate')
+  ->set('.user_photo');
+}
+else
+{
+  echo _media('anonymous.jpg')
+  ->size(300, 300)
+  ->set('.user_photo');
+}
